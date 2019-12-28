fe5981898cea4c9b7b065e1f7a094301ed93cf4d
PriyalsantoshJain <priyaljain2799@gmail.com>
2019-12-28 21:10:21 +0530

    calcy2

---
+++ script.js
@@ -0,0 +1,21 @@
+class Calculator{
+    constructor()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const numberButton = document.querySelectorAll('[data-number]')
+const operationButton = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allclearButton =  document.querySelector('data-all-clear')
+const previousButton = document.q
